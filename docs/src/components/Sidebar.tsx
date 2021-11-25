ac78ba6a7c8630820b056f2d878e0047a64e34a9
hawyar <hawyarfa@gmail.com>
2021-11-25 14:00:58 -0500

    going to delete user stuf for now

--- docs/src/components/Sidebar.tsx
+++ docs/src/components/Sidebar.tsx
@@ -1,47 +1,60 @@
 import {
-  Box,
-  useColorMode,
-  FlexProps,
-  Text,
-  useColorModeValue,
-  Link,
+	Box,
+	useColorMode,
+	useColorModeValue,
+	Link,
 } from "@chakra-ui/react";
+import NextLink from "next/link"
 
-interface Doc {
-  title: string;
-  excerpt: string;
-  coverImage: string;
-  date: string;
-  status: string;
-  author: { name: string; picture: string };
-  ogImage: { url: string };
-  slug: string;
-}
 
-export const Sidebar = ({ docs }: { docs: Array<Doc> }) => {
-  console.log(docs);
-  const { colorMode } = useColorMode();
 
-  const bg = useColorModeValue("gray.100", "gray.900");
+export const Sidebar = ({ docs }) => {
 
-  return (
-    <Box
-      px={5}
-      py={6}
-      w={["100%", "220px"]}
-      minHeight="100vh"
-      position="fixed"
-	  bg={bg}
-	  zIndex={1}
-	  flexShrink={0}
-    >
-      {docs.map((doc: Doc) => {
-        return (
-          <Text key={doc.slug} letterSpacing="-.1px" fontWeight="medium" pt="6">
-            <Link href={`${doc.slug}`}>{doc.title}</Link>
-          </Text>
-        );
-      })}
-    </Box>
-  );
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	const bg = useColorModeValue("gray.50", "gray.800");
+	const color = useColorModeValue("brand.white", "brand.black");
+	const border = useColorModeValue("1px solid #e6e6e6", "1px solid #2D3748");
+
+	return (
+		<Box
+			px={5}
+			py={20}
+			w={["100%", "220px"]}
+			minHeight="100vh"
+			position="fixed"
+			overflow="auto"
+			top="0"
+			bg={bg}
+			left="0"
+			right="0"
+			bottom="0"
+			borderRight={border}
+			zIndex={1}
+			flexShrink={0}
+		>
+			{docs.map((doc: any, i) => {
+				return (
+					<Box mt={i === 0 ? "6" : "12"}>
+						<NextLink key={doc.slug} href={`/docs/${doc.slug}`} passHref>
+							<Link letterSpacing="-.1px" fontWeight="medium" fontSize="lg">{doc.title}</Link>
+						</NextLink>
+
+						<Box mt="4" >
+							{doc.pages.map((page: any) => {
+								return (
+									<Box mt="3">
+										<NextLink key={page.slug} href={`/docs/${page.slug}`} passHref>
+											<Link letterSpacing="-.1px" fontWeight="400" fontSize="16px">{page.title}</Link>
+										</NextLink>
+									</Box>
+								)
+							})}
+						</Box>
+					</Box>
+
+				);
+			})}
+		</Box>
+	);
 };
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,18 +1,19 @@
 import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-  HStack,
-  Heading,
-  Box,
-  SimpleGrid,
-  Stack,
-  Flex,
-  Link,
-  useColorModeValue,
+	Link as ChakraLink,
+	Text,
+	Code,
+	List,
+	ListIcon,
+	ListItem,
+	HStack,
+	Heading,
+	Box,
+	SimpleGrid,
+	Stack,
+	Flex,
+	Link,
+	Grid,
+	useColorModeValue,
 } from "@chakra-ui/react";
 import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
 import { useState, useEffect } from "react";
@@ -21,24 +22,125 @@ import useSWR from "swr";
 import { Sidebar } from "../components/Sidebar";
 import { getAllContents } from "../lib/api";
 import { QuickAccess } from "../components/QuickAccess";
+
 const Index = ({ allContent }) => {
-  //   const fetcher = (args) => fetch(args).then((res) => res.json());
+	//   const fetcher = (args) => fetch(args).then((res) => res.json());
 
-  //   const { data, error } = useSWR("/api/me", fetcher);
+	//   const { data, error } = useSWR("/api/me", fetcher);
 
-  //   if (error) return <div>failed to load</div>;
-  //   if (!data) return <div>loading...</div>;
+	//   if (error) return <div>failed to load</div>;
+	//   if (!data) return <div>loading...</div>;
 
-  // const bg = useColorModeValue("gray.50", "gray.800");
-  // const color = useColorModeValue("brand.white", "brand.black");
-  const bg = useColorModeValue("gray.50", "gray.800");
+	// const bg = useColorModeValue("gray.50", "gray.800");
+	// const color = useColorModeValue("brand.white", "brand.black");
+	const docs = [
+		{
+			title: "Getting Started",
+			slug: "getting-started",
+			pages: [
+				{
+					title: "Installation",
+					slug: "installation",
 
-  return (
-    <div>
-      <Nav />
-      <Stack direction={["column", "row"]} spacing={16} pt={12} bg={bg}>
-        <Sidebar docs={allContent} />
-        <Box bg={bg} pl={56}>
+				},
+				{
+					title: "Usage",
+					slug: "usage",
+				},
+				{
+					title: "Customization",
+					slug: "customization",
+
+				}
+			]
+		},
+		{
+			title: "Components",
+			slug: "components",
+			pages: [
+				{
+					title: "Installation",
+					slug: "installation",
+
+				},
+				{
+					title: "Usage",
+					slug: "usage",
+				},
+				{
+					title: "Customization",
+					slug: "customization",
+
+				}
+			]
+		},
+		{
+			title: "Utilities",
+			slug: "utilities",
+			pages: [
+				{
+					title: "Installation",
+					slug: "installation",
+				},
+				{
+					title: "Usage",
+					slug: "usage",
+
+				},
+				{
+					title: "Dark Mode",
+					slug: "dark-mode",
+				}
+			]
+		},
+		{
+			title: "Core Concepts",
+			slug: "core-concepts",
+			pages: [
+				{
+					title: "Installation",
+					slug: "installation",
+				}
+			]
+		},
+		{
+			title: "Components",
+			slug: "components",
+			pages: [
+				{
+					title: "Installation",
+					slug: "installation",
+
+				},
+				{
+					title: "Usage",
+					slug: "usage",
+				},
+				{
+					title: "Customization",
+					slug: "customization",
+
+				}
+			]
+		},
+	]
+
+	const sideBar = docs.concat(docs)
+
+	// const bg = useColorModeValue("gray.50", "gray.800");
+
+	return (
+		<Box>
+			<Nav />
+			<Sidebar docs={sideBar} />
+			<Grid
+				templateColumns={{ base: "1fr", md: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+				justifyItems="center"
+				alignItems="center"
+			>
+			</Grid>
+			{/* <Sidebar docs={allContent} />
+        <Box  pl={56}>
           <Stack direction="column" spacing="24px">
             <Heading letterSpacing="-1px" size="xl" fontWeight="bold" pt={16}>
               Welcome to the docs!
@@ -75,25 +177,25 @@ const Index = ({ allContent }) => {
               );
             })}
           </Box>
-        </Flex>
-      </Stack>
-    </div>
-  );
+        </Flex> */}
+
+		</Box>
+	);
 };
 
 export default Index;
 
 export async function getStaticProps() {
-  const allContent = getAllContents([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
+	const allContent = getAllContents([
+		"title",
+		"date",
+		"slug",
+		"author",
+		"coverImage",
+		"excerpt",
+	]);
 
-  return {
-    props: { allContent },
-  };
+	return {
+		props: { allContent },
+	};
 }
